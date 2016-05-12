b469eb1ae71b5197e64a841fde4a4b74edbff96d
Mattia Buffa <mattiabuffa@hotmail.it>
2016-05-12 19:30:36 +0200

    ConnectionController GET POST

---
+++ BillOnAirApp/app/src/main/java/com/btd/billonair/ConnectionController.java
@@ -0,0 +1,114 @@
+package com.btd.billonair;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+//import javax.net.ssl.HttpsURLConnection;
+import java.net.HttpURLConnection;
+
+/**
+ * Created by Buffin on 12/05/2016.
+ */
+public class ConnectionController extends AsyncTask<Object, Void, String>
+{
+    URL url;
+    HttpURLConnection connection;
+    DataOutputStream outputStream;
+    String method;
+
+    public ConnectionController(){}
+
+    @Override
+    protected String doInBackground(Object... params) {
+        String response="";
+        this.method = (String)params[0];
+        switch (method)
+        {
+            case "GET":
+                try {
+                    return doGet((String)params[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }break;
+            case "POST":
+                try {
+                    return doPost((String)params[1],(JSONObject)params[2]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }break;
+            case "PUT":break;
+            case "DELETE":break;
+            default: break;
+        }
+        return response;
+    }
+
+    private void InitConnection(String url, String method) throws IOException {
+        this.url = new URL(url);
+        connection = (HttpURLConnection)this.url.openConnection();
+        connection.setReadTimeout(10000);
+        connection.setConnectTimeout(15000);
+        connection.setRequestMethod(method);
+       /* connection.setDoInput(true);
+        connection.setDoOutput(true);*/
+
+        this.method=method;
+    }
+
+    public String doGet(String url) throws IOException {
+        InitConnection(url, "GET");
+        connection.connect();
+        int HttpResult = connection.getResponseCode();
+        StringBuilder sb = new StringBuilder();
+        sb.append("");
+        if (HttpResult == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            String s = null;
+            while ((s = reader.readLine()) != null) {
+                sb.append(s + "\n");
+            }
+            reader.close();
+        }
+        connection.disconnect();
+        return sb.toString();
+    }
+
+    public String doPost(String url, JSONObject json) throws IOException {
+        InitConnection(url, "POST");
+        connection.setDoOutput(true);
+        connection.connect();
+        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+        out.write(json.toString());
+        out.close();
+
+        int HttpResult = connection.getResponseCode();
+        StringBuilder sb = new StringBuilder();
+        sb.append("");
+        if (HttpResult == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            String s = null;
+            while ((s = reader.readLine()) != null) {
+                sb.append(s + "\n");
+            }
+            reader.close();
+        }
+        connection.disconnect();
+        return sb.toString();
+    }
+
+}
---
+++ BillOnAirApp/app/src/main/java/com/btd/billonair/LoginActivity.java
@@ -0,0 +1,140 @@
+package com.btd.billonair;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class LoginActivity extends AppCompatActivity {
+    ViewSwitcher viewSwitcher;
+    LinearLayout layoutLogin;
+    LinearLayout layoutRegister;
+
+    Button btnLogin;
+    Button btnRegister;
+
+    TextView linkRegister;
+    TextView linkLogin;
+    TextView linkOffline;
+    TextView linkOffline2;
+
+    ConnectionController connectionController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        viewSwitcher = (ViewSwitcher)findViewById(R.id.viewSwitcher);
+        layoutLogin = (LinearLayout)findViewById(R.id.layoutLogin);
+        layoutRegister = (LinearLayout)findViewById(R.id.layoutRegister);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+
+        linkRegister = (TextView)findViewById(R.id.linkRegister);
+        linkLogin = (TextView)findViewById(R.id.linkLogin);
+        linkOffline = (TextView)findViewById(R.id.linkOffline);
+        linkOffline2 = (TextView)findViewById(R.id.linkOffline2);
+
+        //logout per prevenire il pulsante back ?
+    }
+
+
+    public void OnClickLink(View v){
+        if((TextView)v==linkLogin)
+        {
+            viewSwitcher.showPrevious();
+        }
+        else
+        if((TextView)v==linkRegister)
+        {
+            viewSwitcher.showNext();
+        }
+        else
+        {
+            //go offline
+            Intent intent = new Intent(this,MainActivity.class);
+            Bundle info = new Bundle();
+            info.putString("mode","offline");
+            intent.putExtra("info",info);
+            startActivity(intent);
+        }
+    }
+
+    public void OnRegister(View v)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        Bundle info = new Bundle();
+        info.putString("mode", "register");
+        intent.putExtra("info",info);
+        //registrazione
+
+        ConnectionController connectionController;
+        connectionController = new ConnectionController();
+        String resp="";
+        try {
+            //resp = connectionController.execute("GET","http://www.surrenderat20.net/").get();
+            //passwrod da cifrare
+            JSONObject json = new JSONObject();
+            json.put("password",((EditText)findViewById(R.id.txtRegisterPassword)).getText());
+            json.put("username", ((EditText) findViewById(R.id.txtRegisterUsername)).getText());
+            if(((EditText)findViewById(R.id.txtRegisterPassword)).getText()==((EditText)findViewById(R.id.txtRegisterConfirmPassword)).getText())
+                resp = connectionController.execute("POST","http://httpbin.org/post",json).get();
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Password non coincidenti",Toast.LENGTH_LONG);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("Debug", resp);
+
+
+        //startActivity(intent);
+    }
+
+    public void OnLogin(View v)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        Bundle info = new Bundle();
+        intent.putExtra("info",info);
+        //login
+        ConnectionController connectionController;
+        connectionController = new ConnectionController();
+        String resp="";
+        try {
+            //resp = connectionController.execute("GET","http://www.surrenderat20.net/").get();
+            //passwrod da cifrare
+            JSONObject json = new JSONObject();
+            json.put("password",((EditText)findViewById(R.id.txtLoginPassword)).getText());
+            json.put("username",((EditText)findViewById(R.id.txtLoginUsername)).getText());
+            resp = connectionController.execute("POST","http://httpbin.org/post",json).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("Debug",resp);
+
+        //startActivity(intent);
+    }
+}
