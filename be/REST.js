613fae42e485f32e0ba0bd7d9b1c4b1785ffb8ea
Buffin <mattiabuffa@hotmail.it>
2016-06-08 16:24:41 +0200

    be update

--- be/REST.js
+++ be/REST.js
@@ -9,12 +9,14 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get("/",function(req,res){
         res.json({"Message" : "Hello World !"});
     });
+    /*
 	router.get("/home",function(req,res){
 		res.json({"Message" : "Homepage!"})
-	});
+	});*/
 
 	router.get('/users',function(req,res){
-		var query = 'select Users.id_user,Users.username from Users';
+        console.log("safdsfdasd");
+		var query = 'select utenti.username from Users';
 		var params = [0];
 		query = mysql.format(query,params);
 		connection.query(query,function(err,rows){
@@ -27,9 +29,11 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
 		});
 	});
 
+    /**************** UTENTI *****************///start
+    
 	router.post('/users/register',function(req,res){
         //console.log('CHIAMATA');
-		var query = "INSERT INTO `billonair`.`users` (`username`, `password`) VALUES (?, ?);";
+		var query = "INSERT INTO `billonair`.`utenti` (`username`, `password`) VALUES (?, ?);";
 		var params = [req.body.username,req.body.password];
 		query = mysql.format(query,params);
 		console.log(query);
@@ -43,7 +47,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
 	});
     
     router.post('/users/login',function(req,res){
-        var query = "SELECT * FROM billonair.users where username=? and password=?;";
+        var query = "SELECT * FROM billonair.utenti where username=? and password=?;";
         var params = [req.body.username,req.body.password];
         query = mysql.format(query,params);
         console.log(query);
@@ -54,6 +58,46 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
                 res.json({'Message':'Login'});
         });
     });
+    
+    //close
+    
+    /**************** QUERIES *****************///start
+    
+    router.post('/users/query',function(req,res){
+        var query = "INSERT INTO billonair.queries (query, timestamp, username) VALUES (?,?,?)";
+        var params = [req.body.query,req.body.timestamp,req.body.username];
+        query = mysql.format(query,params);
+        console.log(query);
+		connection.query(query,function(err,rows){
+			if(err)
+				res.json({'Error':true, 'Message':'Error executing MySQL query', 'err':err});
+			else
+                res.json({'Message':'Query inserita'});
+		});
+    });
+    
+    router.get('/users/:username/query/:timestamp',function(req,res){
+		
+		var query = 'select queries.query from queries where queries.username=? and timestamp>?';
+		var params = [req.params.username,req.params.timestamp];
+		query = mysql.format(query,params);
+		connection.query(query,function(err,rows){
+			if(err)
+				res.json({'Error':true, 'Message':'Error executing MySQL query'});
+			else
+			{
+				res.json(rows);
+			}
+		});
+	});
+    
+    //close
+    
+    /**************** STANZE ******************///start
+    
+    
+    
+    //close
 }
 
 module.exports = REST_ROUTER;
