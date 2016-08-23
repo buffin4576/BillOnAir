dc87716f305c78e09d8bb88ab0c0e62b741b33ab
mattia buffa <mattia buffa>
2016-08-23 17:17:48 +0200

    visualizza debiti in stanza

--- BillOnAirApp/app/src/main/java/com/btd/billonair/DettagliStanza.java
+++ BillOnAirApp/app/src/main/java/com/btd/billonair/DettagliStanza.java
@@ -1,13 +1,16 @@
 package com.btd.billonair;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.app.ActionBar;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,21 +22,50 @@ import java.util.ArrayList;
 public class DettagliStanza extends AppCompatActivity {
     Stanza stanza;
     ArrayList<String> users = new ArrayList<>();
+    SharedPreferences sharedPreferences;
+    String user;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stanza);
+        sharedPreferences = getSharedPreferences("Shared",0);
+        user = sharedPreferences.getString("username","offline");
+
         stanza= (Stanza) getIntent().getSerializableExtra("Stanza");
         LinearLayout linearUsers = (LinearLayout)findViewById(R.id.linearUtentiStanza);
         TextView txtNomeStanza = (TextView)findViewById(R.id.DettagliNomeStanza);
         txtNomeStanza.setText(stanza.getNome());
         users.addAll(stanza.getUsers());
-        for(String user:users){
-            TextView txtUser = new TextView(getApplicationContext());
-            txtUser.setText(user);
+        ArrayList<Object[]> saldi = CalcolaSaldoUtente(stanza.getSpeseStanza(),users,user);
+        for(Object[] o:saldi){
+            TextView txtUser = new TextView(this);
+            txtUser.setText(o[0]+": "+o[1]);
+            txtUser.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
             linearUsers.addView(txtUser);
-            Log.w("Stanza",user);
+            Log.w("Stanza",o[0]+": "+o[1]);
         }
+    }
+
+    private ArrayList<Object[]> CalcolaSaldoUtente(ArrayList<SpesaStanza> spese, ArrayList<String> users, String user){
+        ArrayList<Object[]> totali = new ArrayList<>();
+
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i)!=user){
+                double tot=0;
+                for(int j = 0; j < spese.size(); j++){
+                    if(spese.get(j).getCreditore()==user && spese.get(j).getDebitore()==users.get(i)){
+                        tot+=spese.get(j).getImporto();
+                    }
+                    if(spese.get(j).getCreditore()==users.get(i) && spese.get(j).getDebitore()==user){
+                        tot-=spese.get(j).getImporto();
+                    }
+                }
+                Object[] o = new Object[]{users.get(i),tot};
+                totali.add(o);
+            }
+        }
+
+        return totali;
     }
 
     @Override
