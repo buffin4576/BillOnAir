2202edc4df4783e4af37b2f2a46c648810c95c49
ernrico21 <ernrico21@gmail.com>
2016-08-25 18:04:23 +0200

    xml

---
+++ BillOnAirApp/app/src/main/java/com/btd/billonair/AdapterListaNotifiche.java
@@ -0,0 +1,34 @@
+package com.btd.billonair;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ernrico on 25/08/2016.
+ */
+public class AdapterListaNotifiche extends ArrayAdapter<Notifica>
+{
+    private ArrayList<Conto> LConti;
+    private Context mContext= null;
+    private int mRowLayout;
+    private AdapterListaConti myadapter;
+    private Activity mactivity=null;
+
+    public AdapterListaNotifiche (Activity activity, Context context, int resource,ArrayList<Notifica> objects)
+    {
+        super(context,resource,objects);
+
+
+    }
+    @Override
+    public View getView(final int pos, View view, final ViewGroup parent) {
+        
+        return view;
+    }
+}
---
+++ BillOnAirApp/app/src/main/java/com/btd/billonair/NotificheActivity.java
@@ -0,0 +1,24 @@
+package com.btd.billonair;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by ernrico on 25/08/2016.
+ */
+public class NotificheActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notifiche);
+
+        TextView txtNotifiche=(TextView) findViewById(R.id.txtNomeStanza);
+        ListView lv=(ListView)findViewById(R.id.listanotifiche);
+
+        Stanza s=(Stanza) getIntent().getSerializableExtra("stanza");
+
+
+    }
+}
+
